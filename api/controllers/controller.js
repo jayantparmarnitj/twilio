70236ca4ac991a2a0614a7db6381b79a35053ec3
Sierra 4 <sierra4@OSX4Seirra.local>
2018-01-23 18:14:56 +0530

    Initing

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -12,24 +12,25 @@ try{
   const authToken = 'd569dcea18aa69d7ed19d2dfa35fc6dc';
   const client = require('twilio')(accountSid, authToken);
   const mobile = req.body.data.cardNum;
+  try{
   client.messages.create(
     {
-       //to: '+919813502080',
       to: `${mobile}`,
       from: '+13015473283',
       body: `Greetings! Your OTP is ${code}`,
     },
     (err, message) => {
-     // console.log(message);
       console.log(message.sid);
       if(err)
           res.send(err);
     }
   );
+}
+catch(e)
+{
+  res.send("Enter Registered Mobile number");
+}
 res.send("Otp Sent Successfully");
-
-
-
 }
 catch(e){
   return res.status(500).json({success:0,msg:e.message});
