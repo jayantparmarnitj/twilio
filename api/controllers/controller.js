d30597618a30dc31745e7f147b82814884c6c9e3
Sierra 4 <sierra4@OSX4Seirra.local>
2018-01-23 18:04:38 +0530

    Initing

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -10,8 +10,6 @@ try{
  var code = Math.floor((Math.random()*1000000)+1);
   const accountSid = 'AC5cce3ad6245f9d26361fc1430275274a';
   const authToken = 'd569dcea18aa69d7ed19d2dfa35fc6dc';
- // const code = Math.floor(Math.random() * (max - min + 1)) + min,
-  // require the Twilio module and create a REST client
   const client = require('twilio')(accountSid, authToken);
   const mobile = req.body.data.cardNum;
   client.messages.create(
